f4e84194fddeccfaf13862d83b4c9b749ea301e7
Allen <allenchen08040614@gmail.com>
2019-09-21 13:36:56 +0800

    Initial Commit

---
+++ MinionsSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MinionsSwiftUI
+//
+//  Created by 陳永展 on 2019/9/21.
+//  Copyright © 2019 abc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
